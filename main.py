fc606dd92addc5d843cc9e4487035fc45dba9867
Avanish Gupta <avanishcodes@gmail.com>
2022-10-01 08:55:42 +0530

    Simple GET request with data

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+items = [
+    {'item_name': 'Foo'},
+    {'item_name': 'Bar'},
+    {'item_name': 'Baz'},
+]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return items[skip: skip + limit]
