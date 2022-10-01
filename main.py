686f8f2c52c093117f86df46599f809e1a695a32
Avanish Gupta <56487090+AvanishCodes@users.noreply.github.com>
2022-10-01 09:12:44 +0530

    Simple POST request (#12)

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI, Response, status
 
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    item_name: str
 
 
 items = [
@@ -19,3 +25,9 @@ async def root():
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return items[skip: skip + limit]
+
+
+@app.post("/items/", status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
+    items.append(item)
+    return item
